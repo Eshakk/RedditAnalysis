538dea10529e7a94ffb8249fd838d084c41d2685
Eshak <rsje@rsje>
2015-12-14 08:19:17 -0800

    Sentiment analysis: w_c -> word_classifier, s_a -> sentiment_analysis

---
+++ classifier/s_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import sys
+import mysql.connector
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+fo = open("sentiment_analysis.txt", "w")
+outStr = ""
+currIndex = 0
+
+try:
+    conn = mysql.connector.connect(user='r_acc', password='rsjeejsr', host='127.0.0.1', database='r_db')
+
+    cursor = conn.cursor()
+
+    query = ("select title, subreddit, year from all_threads")
+
+    cursor.execute(query)
+
+    sia = SentimentIntensityAnalyzer()
+    for post_title, subreddit, year in cursor:
+        currIndex += 1
+        modified_title = post_title.encode('utf-8')
+        print str(currIndex) + ": " + modified_title + "\n"
+        outStr = str(year) + "\t" + modified_title + "\t" + subreddit.encode('utf-8')
+        scores = sia.polarity_scores(modified_title)
+        for k in sorted(scores):
+            # compound, negative, neutral, positive
+            outStr += "\t" + str(scores[k])
+            #print ('{0}: {1}, '.format(k, ss[k])),
+        fo.write(outStr + "\n")
+        outStr = ""
+
+    cursor.close()
+    conn.close()
+except:
+    error_value = sys.exc_info()[1]
+    print ('Failed: ',  error_value)
---
+++ classifier/w_c.py
@@ -0,0 +1,76 @@
+__author__ = 'Potula1'
+
+'''
+Loaded data into database r_db;
+Using output files containing information regarding each reddit post into table all_threads, partitioned by year
+load data local infile '/Users/Potula1/Downloads/RedditAnalysis-master/output/output_1450028456.txt' ignore into table all_threads fields terminated by '\t' lines terminated by '\n' (year, title, num_of_comments, num_of_upvotes, source, subreddit);
+'''
+
+import mysql.connector
+from nltk.corpus import stopwords
+import heapq
+import nltk
+
+cnx = mysql.connector.connect(user="r_acc",password="rsjeejsr",host="127.0.0.1",database="r_db")
+cursor = cnx.cursor()
+query = 'select distinct(subreddit) from all_threads'
+cursor.execute(query);
+
+subreddit_topics = []
+for i,j in cursor:
+    print i,j
+    subreddit_topics.append(i)
+
+def remove_stop_words(title):
+    line = ''
+    t = title.split()
+    for word in t:
+        if(word.lower() not in stopwords.words('english')):
+            line += word
+            line += ' '
+    return line
+
+thread = []
+all_words = []
+
+def remove_adjectives(words):
+    line = ''
+    tagged = nltk.pos_tag(words)
+    for i in tagged:
+        if(i[1] not in ['DT','JJ','RB']):
+            print ("Stupid dheeraj")
+
+cnx = mysql.connector.connect(user="r_acc",password="rsjeejsr",host="127.0.0.1",database="r_db")
+cursor = cnx.cursor()
+for i in subreddit_topics:
+    query = 'select title from all_threads where subreddit = "' + str(i) + '";'
+    print query
+    cursor.execute(query)
+    for title in cursor:
+        line = remove_stop_words(title[0])
+        #line = remove_adjectives(line.split())
+        all_words += line.split()
+    thread.append([i,all_words])
+    all_words = []
+
+heap = []
+thread_dict = []
+reddit_dict = {}
+for words_set in thread:
+    for word in words_set[1]:
+        if(reddit_dict.get(word,-1) == -1):
+            reddit_dict[word] = 1
+        else:
+            reddit_dict[word] += 1
+    thread_dict.append([words_set[0],reddit_dict])
+    reddit_dict = {}
+
+heap = []
+for line in thread_dict:
+    print '--------------'
+    print line[0]
+    print
+    for key in line[1]:
+        heapq.heappush(heap,(line[1][key],key))
+    print heapq.nlargest(15,heap)
+    heap = []
